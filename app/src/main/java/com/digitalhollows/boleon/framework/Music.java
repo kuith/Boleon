493769e6d70d7cbca8a75591f7d759386b3a3d47
kuith <kuithsteam@gmail.com>
2015-02-11 17:34:05 +0100

    Añadidas interfaces Music y Sound

---
+++ app/src/main/java/com/digitalhollows/boleon/framework/Music.java
@@ -0,0 +1,16 @@
+package com.digitalhollows.boleon.framework;
+
+/**
+ * Created by Rafa on 11/02/2015.
+ */
+public interface Music {
+    public void play();
+    public void stop();
+    public void pause();
+    public void setLooping(boolean looping);
+    public void setVolume(float volume);
+    public boolean isPlaying();
+    public boolean isStopped();
+    public boolean isLooping();
+    public void dispose();
+}
---
+++ app/src/main/java/com/digitalhollows/boleon/framework/Sound.java
@@ -0,0 +1,10 @@
+package com.digitalhollows.boleon.framework;
+
+/**
+ * Created by Rafa on 11/02/2015.
+ */
+public interface Sound {
+    public void play(float volume);
+
+    public void dispose();
+}
